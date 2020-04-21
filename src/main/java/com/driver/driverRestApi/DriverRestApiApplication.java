49e5306590433bbf02abd96d8ef131409edf6dad
obol007 <obol007@gmail.com>
2020-04-21 13:54:38 +0200

    init

---
+++ src/main/java/com/driver/driverRestApi/DriverRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.driver.driverRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driver/driverRestApi/DriverRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driver.driverRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
