79d9b881f06f994552ce55fd5d07813a0d4b0b20
obol007 <obol007@gmail.com>
2020-04-26 14:20:33 +0200

    home controller

---
+++ src/main/java/com/driver/driverRestApi/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.driver.driverRestApi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping
+    public String start(){
+        return "redirect:/swagger-ui.html";
+    }
+
+
+}
