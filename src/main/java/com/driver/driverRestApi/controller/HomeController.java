0f5878b905be28a40c9207ddd332fc50021425c1
obol007 <obol007@gmail.com>
2020-04-26 14:37:56 +0200

    heroku

--- src/main/java/com/driver/driverRestApi/controller/HomeController.java
+++ src/main/java/com/driver/driverRestApi/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String start(){
         return "redirect:/swagger-ui.html";
     }
