1111426890348337a5b5ff9ddde750d870b3ccbc
obol007 <obol007@gmail.com>
2020-04-27 18:54:58 +0200

    tag endpoints completed

--- src/main/java/com/driver/driverRestApi/controller/TagController.java
+++ src/main/java/com/driver/driverRestApi/controller/TagController.java
@@ -11,12 +11,10 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
+
 
 
 import javax.validation.Valid;
@@ -73,32 +71,31 @@ public class TagController {
 
     @PostMapping
     @ApiOperation(value = "Create a tag")
-    public ResponseEntity<TagResponse> createTag(@Valid @RequestBody TagRequest tagRequest,
-                                                 UriComponentsBuilder builder) {
+    public ResponseEntity<?> createTag(@Valid @RequestBody TagRequest tagRequest) {
         Tag tag = tagConverter.requestToTag(tagRequest);
         Tag tagCreated = tagService.createTag(tag);
-
-        Long id = tagCreated.getId();
-        UriComponents uriComponents = builder.path("/api/tag/{id}").buildAndExpand(id);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(uriComponents.toUri());
-        return new ResponseEntity<>(tagConverter.tagToResponse(tagCreated), headers, HttpStatus.CREATED);
-
+        EntityModel<TagResponse> tagModel = assembler.toModel(tagConverter.tagToResponse(tagCreated));
+        return ResponseEntity
+                .created(tagModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(tagModel);
     }
-
 
     @PutMapping("/{id}")
     @ApiOperation(value = "Update a tag")
-    public TagResponse updateTag(@RequestBody TagRequest tagRequest,
+    public ResponseEntity<?> updateTag(@RequestBody TagRequest tagRequest,
                                  @PathVariable Long id) {
-        Tag tag = tagService.updateTag(tagRequest, id);
-        return tagConverter.tagToResponse(tag);
+        Tag tagUpdated = tagService.updateTag(tagRequest, id);
+        EntityModel<TagResponse> tagModel = assembler.toModel(tagConverter.tagToResponse(tagUpdated));
+        return ResponseEntity
+                .created(tagModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(tagModel);
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation(value = "Delete a tag")
-    public void deleteTag(@PathVariable Long id) {
+    public ResponseEntity<?> deleteTag(@PathVariable Long id) {
         tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 
 
